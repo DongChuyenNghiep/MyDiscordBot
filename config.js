cc4da9f8ec21cadb6d82e4012e4ba9908765d335
DongChuyenNghiep <tuan22000@student.hamk.fi>
2025-03-01 11:55:18 +0200

    update config so that it doesnt run too much

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const GUILD_ID = "1345130738349113344"; 
 export const API_TEAMS = "https://dongchuyennghiep-backend.vercel.app/api/auth/findallteamValorant";
 export const API_USERS = "https://dongchuyennghiep-backend.vercel.app/api/auth/alluser";
-export const CHECK_INTERVAL = 5000;
+export const CHECK_INTERVAL = 15000;
